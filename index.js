4b24032543815a7835da30dfe89cdc3788c66a6b
Salmaan <salmaan.arif.ali@gmail.com>
2021-06-10 04:58:52 -0400

    commit tree c14a6401b44742becc54e54ad1f21261fc85c0ec

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+
+const drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver(driver) {
+  drivers.push(driver);
+}
+
+function destructivelyPrependDriver(driver) {
+  drivers.unshift(driver);
+}
+
+function destructivelyRemoveLastDriver(driver) {
+  drivers.pop();
+}
+
+function destructivelyRemoveFirstDriver(driver) {
+  drivers.shift();
+}
+
+function appendDriver(driver) {
+  // drivers.concat(driver);
+  return [...drivers, driver];
+}
+
+function prependDriver(driver) {
+  return [driver, ...drivers];
+}
+
+function removeLastDriver(driver) {
+  return drivers.slice(0, drivers.length - 1);
+}
+
+function removeFirstDriver(driver) {
+  return drivers.slice(1);
+}
